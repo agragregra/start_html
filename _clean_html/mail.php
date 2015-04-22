37aebaf9f7af9678365ad04ea5324c18a0fc0f8a
Алексей <agragregra@ya.ru>
2015-04-22 21:48:47 +0700

    Update mail.php

--- _clean_html/mail.php
+++ _clean_html/mail.php
@@ -8,5 +8,11 @@ $subject   = "Новая заявка с сайта \"$sitename\"";
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
 $message = trim($_POST["message"]);
-$message = "E-mail: $email \nИмя: $name \nСообщение: $message";
-mail($recepient, $subject, $message, "From: $frm_name <$recepient>" . "\r\n" . "Reply-To: $recepient" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
+
+$message = "
+E-mail: $email <br>
+Имя: $name <br>
+Сообщение: $message
+";
+
+mail($recepient, $subject, $message, "From: $frm_name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
