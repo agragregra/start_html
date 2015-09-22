e95ee7c577eecbdc26c9ab773894723e28ccd52c
Алексей <agragregra@ya.ru>
2015-09-22 10:23:59 +0700

    commit

--- _optimized_gulp_sass/app/mail.php
+++ _optimized_gulp_sass/app/mail.php
@@ -7,12 +7,10 @@ $subject   = "Новая заявка с сайта \"$sitename\"";
 
 $name = trim($_POST["name"]);
 $email = trim($_POST["email"]);
-$message = trim($_POST["message"]);
 
 $message = "
 E-mail: $email <br>
-Имя: $name <br>
-Сообщение: $message
+Имя: $name
 ";
 
 mail($recepient, $subject, $message, "From: $frm_name <$email>" . "\r\n" . "Reply-To: $email" . "\r\n" . "X-Mailer: PHP/" . phpversion() . "\r\n" . "Content-type: text/html; charset=\"utf-8\"");
