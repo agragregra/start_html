a3d65151f1847ed989036bc6b5efc27cf217e16c
Алексей <agragregra@ya.ru>
2016-04-30 11:25:46 +0700

    Update

--- _optimized_gulp_sass/gulpfile.js
+++ _optimized_gulp_sass/gulpfile.js
@@ -34,7 +34,6 @@ gulp.task('scripts', function() {
 		'./app/libs/jquery/jquery-1.11.2.min.js',
 		'./app/libs/waypoints/waypoints.min.js',
 		'./app/libs/animate/animate-css.js',
-		'./app/libs/plugins-scroll/plugins-scroll.js',
 		])
 		.pipe(concat('libs.js'))
 		// .pipe(uglify()) //Minify libs.js
